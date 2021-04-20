9ef907cc279a72a8f5713ec1066717f715bc7f64
tanaka <sisyamo2236@gmail.com>
2021-04-20 14:59:07 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by tanaka  on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
